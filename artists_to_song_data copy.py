ea6929dd8016b56fb1381941b650a759c8ddb2a1
crispianm <45373428+crispianm@users.noreply.github.com>
2022-02-11 12:34:31 +0000

    quick update for transferability

--- artists_to_song_data copy.py
+++ artists_to_song_data copy.py
@@ -25,7 +25,7 @@ albums = []
 album_track_ids = []
 
 for artist_id in artist_ids:
-    print('Searching for artist ' + artist_id)
+    
     album_ids = []
     artist_uri = 'spotify:artist:' + artist_id
 
@@ -38,88 +38,104 @@ for artist_id in artist_ids:
     for album in albums:
         album_ids.append(album['id'])
 
+all_tracks = []
 for id in album_ids:
-    print('Searching for album ' + id)
+
     track_ids = []
 
     results = sp.album_tracks(id)
     tracks = results['items']
+    all_tracks.extend(tracks)
     for track in tracks:
         track_ids.append(track['id'])
 
     album_track_ids.append(track_ids)
 
-# Make sure the correct number of albums is in album_track_ids
+
 assert len(album_track_ids) == len(albums)
 
 
-# Remove unnecessary columns
-columns_to_remove = ['analysis_url',
-                    'type',
-                    'uri',
-                    'track_href']
-
-# Add two new columns, album_title and artist
-headings = ['danceability',
-            'energy',
-            'key',
-            'loudness',
-            'mode',
-            'speechiness',
-            'acousticness',
-            'instrumentalness',
-            'liveness', 
-            'valence', 
-            'tempo', 
-            'id', 
-            'duration_ms', 
-            'time_signature',
-            'album_title',
-            'album_artist']
-
-# Form empty dataframe with these columns
-data = pd.DataFrame(columns=headings)
-
-
-
-## Create DataFrames of features from track ids and compress them into lists so each album is one row in the data matrix
-i = 0
-for album in album_track_ids:
-
-    title = albums[i]['name']
-    name = albums[i]['artists'][0]['name']
-
-    print('Looking up track information for ' + name + ' - ' + title)
-
-    features = []
-    for track in album:
-        results = sp.audio_features(track)
-        # Empty data fix (Glass Animals)
-        if results[0]: 
-            features.append(results[0])
-
-    features_matrix = pd.DataFrame.from_records(features)
-
-    # Remove unneeded columns
-    features_matrix.drop(columns = columns_to_remove, axis = 1, inplace = True)
-
-    feature_lists = []
-    for column in features_matrix:
-        feature_lists.append(list(features_matrix[column]))
-
-    # Add album title
-    feature_lists.append(title)
-    # Add artist name
-    feature_lists.append(name)
-
-    # Add album as a row to data df
-    data.loc[i] = feature_lists
-    i += 1
 
 # Remove any duplicate albums that Spotify might've released multiple versions of
 print('Removing duplicates...')
 data.drop_duplicates(subset=['album_title', 'album_artist'])
 # data[~data.album_title.str.contains("remixes")]
+
+
+# Remove unnecessary columns
+columns_to_remove = ['analysis_url', 'type', 'uri', 'track_href']
+
+# Add 4 new columns, album_title and artist
+headings = ['danceability',
+'energy',
+'key',
+'loudness',
+'mode',
+'speechiness',
+'acousticness',
+'instrumentalness',
+'liveness',
+'valence',
+'tempo',
+'id',
+'duration_ms',
+'time_signature',
+'track_title',
+'album_title',
+'album_artist',
+'track_number',
+'total_tracks']
+
+
+# Form empty dataframe with these columns
+data = pd.DataFrame(columns=headings)
+
+
+## Create DataFrames of features from track ids and compress them into lists so each album is one row in the data matrix
+i = 0
+j = 0
+for album in album_track_ids:
+    
+    length = len(album)
+    
+    
+    for track in album:
+        results = sp.audio_features(track)
+        features = results[0]
+        features_matrix = pd.DataFrame.from_records(features, index=[0])
+
+        # Remove unneeded columns
+        features_matrix.drop(columns = columns_to_remove, axis = 1, inplace = True)
+        # print(features_matrix)
+
+        # Add track title
+        track_title = all_tracks[j]['name']
+        features_matrix['track_title'] = track_title
+        # Add album title
+        album_title = albums[i]['name']
+        features_matrix['album_title'] = album_title
+        # Add artist name
+        name = albums[i]['artists'][0]['name']
+        features_matrix['album_artist'] = name
+
+        # Add track number and total tracks
+        features_matrix['track_number'] = all_tracks[j]['track_number']
+        features_matrix['total_tracks'] = length
+
+        # Add album as a row to the data df
+        # print(features_matrix,'\n')
+        # print(data)
+        data = data.append(features_matrix)
+
+        j += 1
+
+    i += 1
+
+# Remove duplicates
+data.drop_duplicates(subset=['track_number', 'album_title', 'album_artist'])
+
+
+
 
 # Save as csv
 print('Saving as CSV\n')
